cebf3ef27c25c52e4251f50745dc85a1b46e372b
Allen Xiang <allen.xiang@monsanto.com>
2017-03-31 14:02:09 -0500

    remove hardcoded client version.

--- kafka_client.go
+++ kafka_client.go
@@ -73,8 +73,6 @@ func NewKafkaClient(app *ApplicationContext, cluster string) (*KafkaClient, erro
 	}
 	clientConfig.Net.TLS.Config.InsecureSkipVerify = profile.TLSNoVerify
 
-	clientConfig.Version = sarama.V0_10_1_0
-
 	sclient, err := sarama.NewClient(app.Config.Kafka[cluster].Brokers, clientConfig)
 	if err != nil {
 		return nil, err
